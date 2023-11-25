3c4efe20f35361576cd69fd0b49da50d7549407f
davidKirshbom <67784666+davidKirshbom@users.noreply.github.com>
2023-11-25 14:54:58 +0200

    Delete ui directory

--- ui/src/components/Loader.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import styles from "../styles/components/loader.module.scss";
-export default function Loader() {
-  return <div className={styles.ldsDualRing}></div>;
-}
--- ui/src/utils/validator.jsx
+++
@@ -1,6 +0,0 @@
-export function isValidHttpUrl(string) {
-  const urlRegex =
-    /^(?:(?:\w+:)?\/\/([^\s.]+\.\S{2}|localhost[:?\d]*)\S*|\/\S*)$/;
-
-  return urlRegex.test(string);
-}
